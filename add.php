be7a2ea09ae7aa959d80e0e6887e1a8b39a190b4
Toni Leinonen <tonimatias96@gmail.com>
2022-03-17 12:53:58 +0200

    bäkkäri valmis

--- add.php
+++ add.php
@@ -4,14 +4,13 @@ require_once 'inc/functions.php';
 
 $input = json_decode(file_get_contents('php://input'));
 $description = filter_var($input->description,FILTER_SANITIZE_SPECIAL_CHARS);
+$amount = filter_var($input->$amount,FILTER_SANITIZE_NUMBER_INT);
 
 try {
     $db = openDB();
-    
-    $query = $db->prepare('insert into item(description) values (:description)');
-    $query = $db->prepare('insert into item(amout) values (:amount)');
+    $query = $db->prepare('insert into item(description, amount) values (:description, :amount)');
     $query->bindValue(':description',$description, PDO::PARAM_STR);
-    $query->bindValue(':amount',$amount, PDO::PARAM_STR);
+    $query->bindValue(':amount',$amount, PDO::PARAM_INT);
     $query->execute();
 
     header('HTTP/1.1 200 OK');
